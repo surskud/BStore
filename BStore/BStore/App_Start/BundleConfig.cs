469fb4ccea5ec4a51b743e31402ab39a42aef22e
Mark <markaris.trinidad@gmail.com>
2014-08-07 16:26:29 +0800

    All Files

---
+++ BStore/BStore/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace BStore
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/boots").Include(
+                        "~/Content/css/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/Content/struct").Include(
+                        "~/Content/main/home.css"));
+   
+        }
+    }
+}
---
+++ BStore/BStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BStore.MvcApplication" Language="C#" %>
